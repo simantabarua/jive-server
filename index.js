478593523979c5307e29ac458475655929318988
simantabarua <webdevelopersimanta@gmail.com>
2023-06-10 21:26:31 +0600

    delete class and fix repate code

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const dbPass = process.env.DB_PASS;
 const accessToken = process.env.TOKEN;
 
 const jwt = require("jsonwebtoken");
-//port config
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-//middleware
+// Port config
+const port = process.env.PORT || 5000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -25,6 +26,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// Middleware
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -33,7 +36,7 @@ const verifyJWT = (req, res, next) => {
       .send({ error: true, message: "Unauthorized access" });
   }
 
-  // check token
+  // Check token
   const token = authorization.split(" ")[1];
   jwt.verify(token, accessToken, (err, decoded) => {
     if (err) {
@@ -43,9 +46,26 @@ const verifyJWT = (req, res, next) => {
     next();
   });
 };
+
+//Check Access
+
+const checkAccess = (email, decodedEmail, res) => {
+  if (!email) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+
+  console.log(decodedEmail);
+
+  if (email !== decodedEmail) {
+    return res.status(403).send({ error: true, message: "Forbidden access" });
+  }
+};
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     // await client.connect();
     const classesCollection = client.db("jive").collection("classes");
     const instructorCollection = client.db("jive").collection("instructor");
@@ -54,20 +74,31 @@ async function run() {
       .db("jive")
       .collection("selectedClass");
 
-    //generate jwt
+    // Generate JWT
     app.post("/jwt", (req, res) => {
       const body = req.body;
       const token = jwt.sign(body, accessToken, { expiresIn: "1h" });
       res.send(token);
     });
 
-    //users  manage
+    // Load all classes
+    app.get("/classes", async (req, res) => {
+      const classes = await classesCollection.find({}).toArray();
+      res.send(classes);
+    });
 
-    //create user
+    // Load all  instructors
+    app.get("/instructors", async (req, res) => {
+      const classes = await instructorCollection.find({}).toArray();
+      res.send(classes);
+    });
+
+    // ---------------------------------- Manage user start -------------------------------//
+
+    // Create user
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
-      console.log(user.email);
       const isUserExist = await usersCollection.findOne(query);
       console.log("is", isUserExist);
       if (isUserExist) {
@@ -77,17 +108,14 @@ async function run() {
       res.send(result);
     });
 
-    // ---------------------------------- Manage user start -------------------------------//
-    //get all user by admin
+    // Get all users by admin
     app.get("/users", verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { email: email, role: "admin" };
       const isAdmin = await usersCollection.findOne(query);
-      if (req.decoded.email !== email) {
-        return res
-          .status(403)
-          .send({ error: true, message: "Forbidden access" });
-      }
+      const decodedEmail = req.decoded.email;
+      checkAccess(email, decodedEmail, res);
+
       if (!isAdmin) {
         return res
           .status(403)
@@ -97,7 +125,7 @@ async function run() {
       res.send(result);
     });
 
-    // check user
+    // Check user
     app.get("/check-user", verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -105,98 +133,96 @@ async function run() {
       res.send(user.role);
     });
 
-    //change user role
-    app.patch("/change-user-role/:id", async (req, res) => {
+    // Change user role
+    app.patch("/change-user-role/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: req.body.role  
-        }
+          role: req.body.role,
+        },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
-
-    //
-
- // ---------------------------------- Manage user  end-------------------------------//
-
-
-    //load all classes
-    app.get("/classes", verifyJWT, async (req, res) => {
-      const classes = await classesCollection.find({}).toArray();
-      res.send(classes);
     });
-    app.get("/instructor-classes",verifyJWT,  async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        return res
-          .status(401)
-          .send({ error: true, message: "Unauthorized access" });
-      }
-      const decodeEmail = req.decoded.email;
-      console.log(decodeEmail);
 
-      if (email !== decodeEmail) {
-        return res
-          .status(401)
-          .send({ error: true, message: "Forbidden access" });
-      }0
+    // Delete user
+    app.delete("/delete-user/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // ---------------------------------- Manage user end -------------------------------//
+
+    // ---------------------------------- Manage Classes -------------------------------//
+
+    // Get  classes by instructor
+    app.get("/instructor-classes", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      checkAccess(email, decodedEmail, res);
       const query = { email: email };
       const result = await classesCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/instructors", async (req, res) => {
-      const classes = await instructorCollection.find({}).toArray();
-      res.send(classes);
-    });
-    // add class
-    app.post("/add-class", async (req, res) => {
+
+    // Add class
+    app.post("/add-class", verifyJWT, async (req, res) => {
       const newClass = req.body;
       const result = await classesCollection.insertOne(newClass);
       res.send(result);
     });
 
-    // selected class
-    app.post("/selected-class", async (req, res) => {
+    // Change class status
+    app.patch("/change-class/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          classStatus: req.body.status,
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //load  selected class
+    app.post("/selected-class", verifyJWT, async (req, res) => {
       const selectedCardData = req.body;
       console.log(selectedCardData);
       const result = await selectedClassCollection.insertOne(selectedCardData);
       res.send(result);
     });
 
-    // get selected classes
+    // Get selected classes
     app.get("/selected-class", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      console.log(email);
-      if (!email) {
-        return res
-          .status(401)
-          .send({ error: true, message: "Unauthorized access" });
-      }
-      const decodeEmail = req.decoded.email;
-      console.log(decodeEmail);
-
-      if (email !== decodeEmail) {
-        return res
-          .status(401)
-          .send({ error: true, message: "Forbidden access" });
-      }
+      const decodedEmail = req.decoded.email;
+      checkAccess(email, decodedEmail, res);
       const query = { email: email };
       const result = await selectedClassCollection.find(query).toArray();
       res.send(result);
     });
 
-    //delete selected Classes
-    app.delete("/selected-class/:id", async (req, res) => {
+    // Delete class form selected class : student
+    app.delete("/selected-class/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await selectedClassCollection.deleteOne(query);
       res.send(result);
     });
 
-    // add class
+    // Delete class for db : admin/instructor
+    app.delete("/delete-class/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ---------------------------------- Manage Classes end -------------------------------//
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -208,6 +234,7 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
